b20334e2ff166aba615a5cccf859d0f9e50bda0d
quackideas <quannguyen@Quans-MacBook-Pro.local>
2018-03-18 13:32:17 -0700

    bug fix travis

--- getPage.js
+++ getPage.js
@@ -1,9 +1,4 @@
-language: node_js
-node_js:
-    - node
-cache:
-    directories:
-        - node_modules
-script:
-    - npm install -g serverless
-    - sls deploy
+exports.handler = (event, context, callback) =>
+{
+
+}
