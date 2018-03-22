cd8b049843078d103e896e85e53d6184c360b99b
quackideas <quannguyen@Quans-MacBook-Pro.local>
2018-03-22 16:25:38 -0700

    add aws dependency

--- meetings.js
+++ meetings.js
@@ -1,10 +1,18 @@
+var AWS = require('aws-sdk');
+
+// Get reference to AWS clients
+// var docClient = new AWS.DynamoDB.DocumentClient({
+//     region:process.env.DB_REGION
+// });
+
+
 exports.handler = (event, context, callback) =>
 {
-    callback(null,{
+    callback(null, {
         "statusCode": 200,
         "headers": {
             "Access-Control-Allow-Origin": "*"
         },
-       "body" : "test"
+       "body" : "{\"test\": \"here\"}"
     });
 }
