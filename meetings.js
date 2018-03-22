2a28f1ac55dd6a6a8026e1cc54874e7a6de2b774
quackideas <quannguyen@Quans-MacBook-Pro.local>
2018-03-22 16:13:59 -0700

    bump

--- meetings.js
+++ meetings.js
@@ -1,6 +1,10 @@
 exports.handler = (event, context, callback) =>
 {
     callback(null,{
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
        "body" : "test"
     });
 }
