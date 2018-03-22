015778ba82bb462f1a8ff4d38902d5a4e776d2f2
quackideas <quannguyen@Quans-MacBook-Pro.local>
2018-03-22 15:12:50 -0700

    update lambda

--- meetings.js
+++ meetings.js
@@ -1,16 +1,6 @@
 exports.handler = (event, context, callback) =>
 {
     callback(null,{
-       events: [
-           {
-               'name': '2018 Assembly'
-           },
-           {
-               'name': '2017 Assembly'
-           },
-           {
-               'name': '2016 Assembly'
-           }
-       ]
+       "body" : "test"
     });
 }
